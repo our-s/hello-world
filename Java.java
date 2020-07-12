30e799e09bfac27ad3270e23fb9b3bfd3f67c3b8
our-s <63934022+our-s@users.noreply.github.com>
2020-07-12 13:46:22 -0300

    Hello world in Java

---
+++ Java.java
@@ -0,0 +1,7 @@
+/* Hello world in Java */
+
+public class Java {
+    public static void main (String args[]) { // or public static void main (String[] args)
+        System.out.println("Hello World!");
+    }
+}
