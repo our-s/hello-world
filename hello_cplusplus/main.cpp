6f9f95f63418daf41375eb43318ece595704c3b0
our-s <63934022+our-s@users.noreply.github.com>
2021-01-03 10:28:05 -0300

    Hello world in C++

---
+++ hello_cplusplus/main.cpp
@@ -0,0 +1,11 @@
+//
+//  main.cpp
+//  hello_cplusplus
+//
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!!\n\nTrying new things... \nHappy New Year!🎆 \n\n";
+    return 0;
+}
